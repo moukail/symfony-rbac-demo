081ca8eb9f854c55850d1625e446d331a9452eb2
Ismail Moukafih <i.moukafih@gmail.com>
2023-11-19 21:58:42 +0100

    twig templates

--- web/src/Form/RoleType.php
+++ web/src/Form/RoleType.php
@@ -6,6 +6,7 @@ use App\Entity\Permission;
 use App\Entity\Role;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -14,12 +15,23 @@ class RoleType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('name')
+            ->add('name', TextType::class, [
+                //'label' => 'Name',
+                'attr' => [
+                    'placeholder' => 'Name',
+                ],
+                'row_attr' => [
+                    'class' => 'form-floating',
+                ],
+            ])
             ->add('permissions', EntityType::class, [
                 'class' => Permission::class,
                 'multiple' => true,
                 'expanded' => true,
                 'choice_label' => 'label',
+                'label_attr' => [
+                    'class' => 'checkbox-inline checkbox-switch'
+                ],
             ])
         ;
     }
